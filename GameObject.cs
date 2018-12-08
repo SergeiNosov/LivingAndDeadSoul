ea6cdbba18661a9d75030036a495e84af92cfb9b
Sergei <sergeinosov6@icloud.com>
2018-12-08 06:17:46 +0300

    worked MapView

---
+++ GameObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace LivingAndDeadSoul
+{
+    public abstract class GameObject
+    {
+        public Vector2 position;
+        public Texture2D texture;
+        public string textureName;
+      
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+        public abstract void LoadContent(Game  game, int idType);
+        public void RightDirection(){
+            position.X += 1;
+        }
+    }
+  
+
+}
---
+++ GameObjects/PlGirl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using LivingAndDeadSoul.HelperClass;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+namespace LivingAndDeadSoul.GameObjects
+{
+    public class PlGirl:GameObject
+    {
+        public int Size = 64;
+        public bool IsSolid = true;
+       
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            var x = Convert.ToInt32(position.X);
+            var y = Convert.ToInt32(position.Y);
+            Rectangle destinationRectangle = new Rectangle(x, y, 64, 64);
+            spriteBatch.Draw(texture, destinationRectangle, Color.White);
+
+
+        }
+        public override void LoadContent(Game game, int idType)
+        {
+
+            DataTypeScene type = new DataTypeScene();
+            if(textureName!="PlayerGirl"&&textureName!="PlayerMan")
+            game.Content.RootDirectory = "Content/" + type.GetTypeMode(idType);
+            else {
+                game.Content.RootDirectory = "Content/Players";
+            }
+            texture = game.Content.Load<Texture2D>(textureName);
+
+        }
+       
+    }
+}
