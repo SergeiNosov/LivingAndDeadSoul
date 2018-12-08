4633124ce6bbb649201bb19a789981e07c728728
Sergei <sergeinosov6@icloud.com>
2018-12-09 01:25:47 +0300

    Add ExitLvl fix

--- GameObjects/ExitLvl.cs
+++ GameObjects/ExitLvl.cs
@@ -1,10 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using LivingAndDeadSoul.HelperClass;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 namespace LivingAndDeadSoul.GameObjects
 {
-    public class ExitLvl
+    public class ExitLvl:GameObject
     {
-        public ExitLvl()
+
+        public int Size = 64;
+        public int Width = 64;
+        public int Height = 64;
+        public bool IsSolid = true;
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
+            var x = Convert.ToInt32(position.X);
+            var y = Convert.ToInt32(position.Y);
+            destinationRectangle = new Rectangle(x - (Width - Size), y - (Height - Size), Width, Height);
+            spriteBatch.Draw(texture, destinationRectangle, Color.White);
+
+
+        }
+        public override void LoadContent(Game game, int idType)
+        {
+
+            DataTypeScene type = new DataTypeScene();
+
+            game.Content.RootDirectory = "Content/" + type.GetTypeMode(idType);
+
+            texture = game.Content.Load<Texture2D>(textureName);
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
         }
     }
 }
